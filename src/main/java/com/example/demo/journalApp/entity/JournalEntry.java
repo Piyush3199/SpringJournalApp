a169a9c3f34f2bb14885a7f743b05ab2bd81a4db
Piyush3199 <67213994+Piyush3199@users.noreply.github.com>
2024-08-30 19:46:33 +0530

    Added Project Lombok

--- src/main/java/com/example/demo/journalApp/entity/JournalEntry.java
+++ src/main/java/com/example/demo/journalApp/entity/JournalEntry.java
@@ -5,10 +5,17 @@ import java.sql.Date;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 
 @Document(collection = "journal_entries")
+@Getter
+@Setter
+//Can use @Data to avoid indivisual getters and setters
 public class JournalEntry {
 	
 	@Id // Created a primary key
@@ -17,29 +24,5 @@ public class JournalEntry {
 	private String content;
 	private LocalDateTime date;
 	
-	public LocalDateTime getDate() {
-		return date;
-	}
-	public void setDate(LocalDateTime date) {
-		this.date = date;
-	}
-	public ObjectId getId() {
-		return id;
-	}
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
 	
 }
